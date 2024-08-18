d1eacf141ac1476bf550254010e5310c9a2ea844
Javid <sairexm@gmail.com>
2024-08-18 19:16:18 +0400

    Final Commit

--- src/main/java/com/calculator/demo/CalculatorController.java
+++ src/main/java/com/calculator/demo/CalculatorController.java
@@ -9,30 +9,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
-    public CalculatorController(CalculatorService calculatorService){
+
+    public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
+
     @GetMapping
-    public String welcome(){
+    public String welcome() {
         return calculatorService.welcome();
     }
 
     @GetMapping(path = "/plus")
-    public String calcPlus(@RequestParam(name ="num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
-       return calculatorService.calcPlus(num1, num2);
+    public String calcPlus(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
+        return calculatorService.calcPlus(num1, num2);
     }
+
     @GetMapping(path = "/minus")
-    public String calcMinus(@RequestParam(name ="num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
+    public String calcMinus(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
         return calculatorService.calcMinus(num1, num2);
     }
 
     @GetMapping(path = "/plus")
-    public String calcMultiply(@RequestParam(name ="num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
+    public String calcMultiply(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
         return calculatorService.calcMultiply(num1, num2);
     }
 
     @GetMapping(path = "/plus")
-    public String calcDivide(@RequestParam(name ="num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
+    public String calcDivide(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
         return calculatorService.calcDivide(num1, num2);
     }
 }
